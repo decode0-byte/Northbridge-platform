3d0c00828fbcc85f3b2974935cd6804ba6ffef26
Blink <keenes656@gmail.com>
2026-05-05 15:38:43 -0800

    Initial clean commit

---
+++ server.js
@@ -0,0 +1,469 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(session({
+    secret: "secretkey",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/uploads", express.static("uploads"));
+
+app.use(express.static("public"));
+
+app.use("/uploads", express.static("uploads"));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+const transporter = nodemailer.createTransport({
+
+    service: "gmail",
+
+    auth: {
+
+user: process.env.EMAIL_USER,
+pass: process.env.EMAIL_PASS
+    }
+});
+
+const applicantSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    cv: String,
+    job: String,
+
+    status: {
+        type: String,
+        default: "Pending"
+    },
+
+     notes: {
+    type: String,
+    default: ""
+},
+
+interviewDate: {
+    type: String,
+    default: ""
+}
+
+});
+
+const Applicant = mongoose.model("Applicant", applicantSchema);
+const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+async function createAdmin() {
+
+    const existingAdmin =
+        await Admin.findOne({
+            username: "admin"
+        });
+
+    if (!existingAdmin) {
+
+        const hashedPassword =
+            await bcrypt.hash("admin123", 10);
+
+        const admin = new Admin({
+            username: "admin",
+            password: hashedPassword
+        });
+
+        await admin.save();
+
+        console.log("Admin created");
+    }
+}
+
+createAdmin();
+
+const storage = multer.diskStorage({
+
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage
+});
+
+app.post("/apply", upload.single("cv"), async (req, res) => {
+
+    const applicant = {
+        name: req.body.name,
+        email: req.body.email,
+        cv: req.file.filename,
+        job: req.body.job
+    };
+
+    const newApplicant = new Applicant(applicant);
+
+    await newApplicant.save();
+
+    await transporter.sendMail({
+
+    from: "keenes656@gmail.com",
+
+    to: req.body.email,
+
+    subject: "Application Received",
+
+html: `
+
+<div style="
+    font-family: Arial;
+    max-width: 600px;
+    margin: auto;
+    padding: 30px;
+    background: #f4f4f4;
+">
+
+    <div style="
+        background: white;
+        padding: 40px;
+        border-radius: 15px;
+    ">
+
+        <h1 style="
+            color: #222;
+            text-align: center;
+        ">
+            Northbridge Solutions
+        </h1>
+
+        <hr>
+
+        <h2>
+            Application Received
+        </h2>
+
+        <p>
+            Hello ${req.body.name},
+        </p>
+
+        <p>
+            Thank you for applying for the
+            <strong>${req.body.job}</strong>
+            position at Blink Careers.
+        </p>
+
+        <p>
+            Our recruitment team has successfully
+            received your application and CV.
+        </p>
+
+        <p>
+            Your application is currently under review.
+            If shortlisted, you will be contacted
+            for the next stage of the hiring process.
+        </p>
+
+        <div style="
+            background: #f9f9f9;
+            padding: 20px;
+            border-radius: 10px;
+            margin-top: 20px;
+        ">
+
+            <h3>Application Details</h3>
+
+            <p>
+                <strong>Name:</strong>
+                ${req.body.name}
+            </p>
+
+            <p>
+                <strong>Email:</strong>
+                ${req.body.email}
+            </p>
+
+            <p>
+                <strong>Position:</strong>
+                ${req.body.job}
+            </p>
+
+            <p>
+                <strong>Status:</strong>
+                Pending Review
+            </p>
+
+        </div>
+
+        <p style="
+            margin-top: 30px;
+        ">
+            We appreciate your interest in joining
+            Northbridge Solutions and wish you success
+            throughout the recruitment process.
+        </p>
+
+        <p>
+            Best regards,
+            <br>
+            Northbridge Solutions Recruitment Team
+        </p>
+
+    </div>
+
+</div>
+
+`
+
+});
+
+    res.send("Application received!");
+});
+
+app.get("/applicants", async (req, res) => {
+
+    const applicants = await Applicant.find();
+
+    res.json(applicants);
+});
+
+app.delete("/applicants/:id", async (req, res) => {
+
+    await Applicant.findByIdAndDelete(req.params.id);
+
+    res.send("Applicant deleted");
+});
+
+app.put("/applicants/:id", async (req, res) => {
+
+    const applicant =
+        await Applicant.findByIdAndUpdate(
+
+            req.params.id,
+
+            {
+                status: req.body.status
+            },
+
+            {
+                new: true
+            }
+        );
+
+    await transporter.sendMail({
+
+        from: "keenes656@gmail.com",
+
+        to: applicant.email,
+
+        subject: "Application Status Updated",
+
+        html: `
+
+        <div style="
+            font-family: Arial;
+            max-width: 600px;
+            margin: auto;
+            padding: 30px;
+            background: #f4f4f4;
+        ">
+
+            <div style="
+                background: white;
+                padding: 40px;
+                border-radius: 15px;
+            ">
+
+                <h1>Northbridge Solutions</h1>
+
+                <hr>
+
+                <h2>
+                    Application Status Update
+                </h2>
+
+                <p>
+                    Hello ${applicant.name},
+                </p>
+
+                <p>
+                    Your application for the
+                    <strong>${applicant.job}</strong>
+                    role has been updated.
+                </p>
+
+                <div style="
+                    background: #f9f9f9;
+                    padding: 20px;
+                    border-radius: 10px;
+                    margin-top: 20px;
+                ">
+
+                    <h3>
+                        Current Status:
+                    </h3>
+
+                    <p style="
+                        font-size: 22px;
+                        font-weight: bold;
+                    ">
+                        ${applicant.status}
+                    </p>
+
+                </div>
+
+                <p style="
+                    margin-top: 25px;
+                ">
+                    Thank you for your interest
+                    in Northbridge Solutions.
+                </p>
+
+            </div>
+
+        </div>
+
+        `
+    });
+
+    res.send("Status updated");
+});
+
+app.put("/notes/:id", async (req, res) => {
+
+    await Applicant.findByIdAndUpdate(
+
+        req.params.id,
+
+        {
+            notes: req.body.notes
+        }
+    );
+
+    res.send("Notes saved");
+});
+
+app.put("/interview/:id", async (req, res) => {
+
+    const applicant =
+        await Applicant.findByIdAndUpdate(
+
+            req.params.id,
+
+            {
+                interviewDate:
+                    req.body.interviewDate
+            },
+
+            {
+                new: true
+            }
+        );
+
+    await transporter.sendMail({
+
+        from: "keenes656@gmail.com",
+
+        to: applicant.email,
+
+        subject: "Interview Scheduled",
+
+        html: `
+
+        <h1>Northbridge Solutions</h1>
+
+        <p>
+            Hello ${applicant.name},
+        </p>
+
+        <p>
+            Your interview for the
+            <strong>${applicant.job}</strong>
+            role has been scheduled.
+        </p>
+
+        <h2>
+            ${applicant.interviewDate}
+        </h2>
+
+        <p>
+            Please be available at the
+            scheduled time.
+        </p>
+
+        `
+    });
+
+    res.send("Interview scheduled");
+});
+
+app.post("/login", async (req, res) => {
+
+    const { username, password } = req.body;
+
+    const admin = await Admin.findOne({
+        username: username
+    });
+
+    if (!admin) {
+        return res.send("Admin not found");
+    }
+
+    const validPassword =
+        await bcrypt.compare(
+            password,
+            admin.password
+        );
+
+    if (!validPassword) {
+        return res.send("Wrong password");
+    }
+
+    req.session.admin = admin;
+
+    res.send("Login successful");
+});
+
+app.get("/", (req, res) => {
+
+    res.sendFile(
+        __dirname + "/public/index.html"
+    );
+});
+
+app.get("/logout", (req, res) => {
+
+    req.session.destroy();
+
+    res.redirect("/admin.html");
+});
+
+app.get("/dashboard", (req, res) => {
+
+    if (!req.session.admin) {
+
+        return res.redirect("/admin.html");
+    }
+
+    res.sendFile(
+        __dirname + "/public/dashboard-private.html"
+    );
+});
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
