17d5dcbe4696477e2497279a0f3b9633fd42e170
Blink <keenes656@gmail.com>
2026-05-15 07:39:28 -0800

    Fixed Gmail SMTP

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ require("dotenv").config();
 const transporter =
 nodemailer.createTransport({
 
-    service: "gmail",
+    host: "smtp.gmail.com",
+
+    port: 465,
+
+    secure: true,
 
     auth: {
 
