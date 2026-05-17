447eb777e3770dccac0910c9dc6dc30f7310a386
Blink <keenes656@gmail.com>
2026-05-17 14:53:01 -0800

    Switched SMTP to Brevo

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 const transporter =
 nodemailer.createTransport({
 
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
 
     port: 587,
 
@@ -23,15 +23,10 @@ nodemailer.createTransport({
     auth: {
 
         user:
-        process.env.EMAIL_USER,
+        process.env.BREVO_USER,
 
         pass:
-        process.env.EMAIL_PASS
-    },
-
-    tls: {
-
-        rejectUnauthorized: false
+        process.env.BREVO_PASS
     }
 });
 
